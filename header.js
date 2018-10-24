8377fe5f1845cd80d9091a3388575a6c2892a544
llovo31 <lmlovo94@hotmail.com>
2018-10-24 14:43:58 -0400

    Working on submitting forms

---
+++ header.js
@@ -0,0 +1,20 @@
+$("document").ready(function() {
+
+    //declare variable path as file path with file name and type
+    var path = window.location.pathname;
+
+    //Reduce string to file name and type
+    var page = path.split("/").pop();
+
+    //Reduce string to file name
+    var fileName = page.split(".").shift();
+    
+    //Do only if page is not index
+    if ( fileName != "index") {
+
+        //change css
+        document.getElementById(fileName).style.color = "#6200EE";
+        document.getElementById(fileName).style.fontWeight = "600";
+
+    }
+});
