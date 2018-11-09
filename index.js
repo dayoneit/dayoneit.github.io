ab6dfa1b88ac6e2c97192d9a88507a4037a2c32c
llovo31 <lmlovo94@hotmail.com>
2018-11-09 12:43:14 -0500

    Pricing buttons popups working and sending emails, fixes spacing in value section

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ $("document").ready(function() {
     $('#header').load('header.html'); 
     $('#footer').load('footer.html'); 
     $('#modal1').load('modal.html');
-    $('#modalGetStarted').load('modal.html');
+    $('#modal-GetStarted').load('modal-getStarted.html');
 
     $('#modal1').on('show.bs.modal', function (event) {
         let button = $(event.relatedTarget) // Button that triggered the modal
         let recipient = button.data('whatever') // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
         let modal = $(this)
         // modal.find('.modal-title').text('Great! Let’s make sure we can contact you! ')
         
@@ -62,45 +61,64 @@ $("document").ready(function() {
         $('#exampleModalLabel').show();
         $('#modalSubTitle').show();
     });
+    
 
     // Get Started PopUP
-    $('#modalGetStarted').on('show.bs.modal', function (event) {
+    $('#modal-GetStarted').on('show.bs.modal', function (event) {
         let button = $(event.relatedTarget) // Button that triggered the modal
         let recipient = button.data('whatever') // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
         let modal = $(this)
-        modal.find('.modal-title').text('Request an appointment')
-        modal.find('.modal-subTitle').text('We’ll send someone over to your home to help you out!')
-        modal.find('#submit').text('Request')
+        // modal.find('.modal-title').text('Request an appointment')
+        // modal.find('.modal-subTitle').text('We’ll send someone over to your home to help you out!')
+        // modal.find('#submit1').text('Request')
         
         $('#contact_form').on('submit', function(e){
             let postData = $(this).serializeArray();
             let formURL = $(this).attr('action');
             let $completeForm = $newdiv1 = $( "<div class='text-center'><img src='img/completeImg.png' /></div>" );
-            $.ajax({
-                type: "POST",
-                url: formURL,
-                data: postData,
-                success: function(data, textStatus, jqXHR){
-                    $('#exampleModal .modal-header .modal-title').html('Got it! We will let you know as soon as we have an update.');
-                    $('#exampleModal .modal-header .modal-subTitle').remove();
-                    $('#exampleModal .modal-body').html(data);
-                    $('#exampleModal .modal-body').append( $completeForm );
-                    // $('#submit').remove();
-                    $('#submit').html('Return');
-                    $('#submit').attr('data-dismiss','modal');
-                    $('#submit').attr('aria-label', 'Close');
-                },
-                error: function(jqXHR, status, error){
-                    console.log(status + ": " + error);
-                }
-            });
-            e.preventDefault();
+            console.log("Helloooooo");
+        //     // $.ajax({
+        //     //     type: "POST",
+        //     //     url: formURL,
+        //     //     data: postData,
+        //     //     success: function(data, textStatus, jqXHR){
+        //     //         $('#exampleModal .modal-header .modal-title').html('Got it! We will let you know as soon as we have an update.');
+        //     //         $('#exampleModal .modal-header .modal-subTitle').remove();
+        //     //         $('#exampleModal .modal-body').html(data);
+        //     //         $('#exampleModal .modal-body').append( $completeForm );
+        //     //         // $('#submit').remove();
+        //     //         $('#submit').html('Return');
+        //     //         $('#submit').attr('data-dismiss','modal');
+        //     //         $('#submit').attr('aria-label', 'Close');
+        //     //     },
+        //     //     error: function(jqXHR, status, error){
+        //     //         console.log(status + ": " + error);
+        //     //     }
+        //     // });
+        //     // e.preventDefault();
         });
-        // $('#submit').on('click', function(){
-        //     $('#contact_form').submit();
-        // });
+        $('#submit1').on('click', function(){
+            $('#thankYou2').show();
+            $('#thankYou22').show();
+            $('#return1').show();
+            $('#contact_form1').hide();
+            $('#modalGetStartedLabel').hide();
+            $('#modalSubTitle1').hide();
+        });
+    });
+    //For Get Started
+    $('body').on('hidden.bs.modal', '.modal', function () {
+        $('#modalGetStarted form :input').val("");
+        $( "#11" ).prop( "checked", false ); 
+        $( "#22" ).prop( "checked", false ); 
+        $( "#33" ).prop( "checked", false );   
+        $('#thankYou2').hide();
+        $('#thankYou22').hide();
+        $('#return1').hide();
+        $('#contact_form1').show();
+        $('#modalGetStartedLabel').show();
+        $('#modalSubTitle1').show();
     });
 });
 
