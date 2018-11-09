9508943325e47b9855e1e97a2595c868ac8783c3
Brandon Garcia <grandonleebarcia@gmail.com>
2018-11-09 11:38:30 -0500

    Update everything

--- header.js
+++ header.js
@@ -9,11 +9,16 @@ $("document").ready(function() {
 
     //Reduce string to file name
     var fileName = page.split(".").shift();
+    console.log(fileName);
 
 
     //Do only if page is not index
     if ( fileName != "index") {
 
+        if ( fileName == "apply") {
+          document.getElementById('techie').style.borderBottom = "solid #6200EE";
+          document.getElementById('techie').style.padding = "15px 0px 10px 0px";
+        }
         //change css
 
         document.getElementById(fileName).style.borderBottom = "solid #6200EE";
