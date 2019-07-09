041076d715c64683ba63224e8c5b271252ee7f49
Brandon Garcia <grandonleebarcia@gmail.com>
2019-07-09 10:32:44 -0400

    use jwt to access user profile in edit.html and edit.js

--- edit.js
+++ edit.js
@@ -1,8 +1,50 @@
+const form = document.querySelector('form');
+const api = 'http://localhost:8080/edit'
+
+
+form.addEventListener('submit', (event) =>{
+  event.preventDefault();
+  const formData = new FormData(form);
+  const email = formData.get('email');
+  const pass =  formData.get('password');
+
+
+
+  const data = {
+    email,
+    pass
+  };
+
+  localStorage.setItem('email', data.email);
+
+  fetch(api, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+        'content-type': 'application/json'
+    }
+  }).then(response => response.json())
+    .then(data => {
+          console.log(data);
+          localStorage.setItem('jwt', data.token );
+
+
+          window.location.href = "admin";
+
+    });
+
+
+
+});
+
+
 function fillFields() {
   const api = 'http://localhost:8080/edit';
   let user = localStorage.getItem('email');
+  let jwt = localStorage.getItem('jwt');
   let body = {
     user: user,
+    jwt: jwt
   }
 
   if(jwt){
@@ -18,13 +60,14 @@ function fillFields() {
       headers: {
         'Content-Type' : 'application/json',
       }
-    }).then(respose => response.json())
+    }).then(response => response.json())
       .then(data => {
-            document.getElementById('email').value = "hello";
-            document.getElementById('email').value = "hello";
-            document.getElementById('email').value = "hello";
-            document.getElementById('email').value = "hello";
-            document.getElementById('email').value = "hello";
-            document.getElementById('email').value = "hello";
+            document.getElementById('email').value = data.email;
+            document.getElementById('fName').value = data.fName;
+            document.getElementById('lName').value = data.lName;
+            document.getElementById('phone').value = data.phone || " ";
+            document.getElementById('address').value = data.address || " ";
+            document.getElementById('city').value = data.city || " ";
+            document.getElementById('zip').value = data.zip || " ";
       });
 }
