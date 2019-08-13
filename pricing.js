c30fd1856848aa0d34cfad89a454456b13f7e81c
Brandon Garcia <grandonleebarcia@gmail.com>
2019-08-13 10:30:14 -0400

    added confirmation.html

--- pricing.js
+++ pricing.js
@@ -14,3 +14,30 @@ function square() {
 
 
 }
+
+function redirectToCheckout() {
+  var stripe = Stripe('pk_test_1xc0fE9f2HobvpJWiTI2tT4j00RZM8Ghs0');
+
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      items: [{plan: 'plan_abc123', quantity: 1}],
+
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://orchardinfo.com/pricing',
+      cancelUrl: 'https://orchardinfo.com/',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+
+}
